f431028119c975760dd31fef3ac847ceb07b01cd
DmJSCourse <dimonua@gmail.com>
2019-07-21 03:31:54 +0300

    v2

--- L8.js
+++ L8.js
@@ -10,20 +10,25 @@ $(document).ready(() => {
 });
 
 function buildTable(received) {
-
-    console.log(received, name);
     let htmlCode = '<table class="table"><thead><tr><th>название страны</th><th>столица</th><th>населениe</th><th>площадь</th><th>валюты</th><th>языки</th><th>флаг</th><th>граничит с</th></tr></thead><tbody>';
     for (let element of received) {
         htmlCode += `<tr><td>${element.name}</td><td>${element.capital}</td><td>${element.population}</td><td>${element.area}</td><td>`;
-        for (let listCurrencies = 0; listCurrencies < element.currencies.length; listCurrencies++) { htmlCode += `${element.currencies[listCurrencies].name}, ` };
+        for (let listCurrencies = 0; listCurrencies < element.currencies.length; listCurrencies++) {
+            htmlCode += `${element.currencies[listCurrencies].name}`;
+            if (listCurrencies + 1 < element.currencies.length) { htmlCode += ', ' };
+        };
         htmlCode += `</td><td>`;
-        for (let listLanguages = 0; listLanguages < element.languages.length; listLanguages++) { htmlCode += `${element.languages[listLanguages].name}, ` };
+        for (let listLanguages = 0; listLanguages < element.languages.length; listLanguages++) {
+            htmlCode += `${element.languages[listLanguages].name}`;
+            if (listLanguages + 1 < element.languages.length) { htmlCode += ', ' };
+        };
         htmlCode += `</td><td><img src="${element.flag}"></td><td>`;
         for (let listBorders = 0; listBorders < element.borders.length; listBorders++) {
             var a3code = element.borders[listBorders];
             for (let searchArray = 0; searchArray < received.length; searchArray++) {
                 if (received[searchArray].alpha3Code === a3code) {
-                    htmlCode += `${received[searchArray].name}, `;
+                    htmlCode += `${received[searchArray].name}`;
+                    if (listBorders + 1 < element.borders.length) { htmlCode += ', ' };
                 };
             };
         };
